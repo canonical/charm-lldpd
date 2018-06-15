890e5d4a9f7a332d2055874e9758a3aa74bdc781
Ante Karamatic <ante.karamatic@canonical.com>
2018-06-15 09:41:49 +0200

    Adjust for new charmhelpers location

--- hooks/hooks.py
+++ hooks/hooks.py
@@ -14,10 +14,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from charmhelpers.core import hookenv, host
-from charmhelpers import fetch
 import os
 import sys
+
+_path = os.path.dirname(os.path.realpath(__file__))
+_parent = os.path.abspath(os.path.join(_path, ".."))
+
+
+def _add_path(path):
+    if path not in sys.path:
+        sys.path.insert(1, path)
+
+
+_add_path(_parent)
+
+from charmhelpers.core import hookenv, host
+from charmhelpers import fetch
 
 PACKAGES = ['lldpd']
 
